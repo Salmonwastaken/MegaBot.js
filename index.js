731f9d58509ace6911f75317baad08419959c76b
Salmonwastaken <gerdingjeremy@gmail.com>
2022-05-03 18:52:01 +0200

    add spotbot functionality

--- index.js
+++ index.js
@@ -7,8 +7,16 @@ const {token,
   spotifyID,
   spotifySecret,
   spotifyRefresh,
-  steamedcatsID} = require( `/etc/Projects/MegaBot/vars.json` );
+  steamedcatsID,
+  globalInterval,
+  dropboxRefresh,
+  dropboxClientId,
+  dropboxClientSecret,
+  mascotchannelId,
+  dropboxFolder,
+  danId} = require( `/etc/Projects/MegaBot/vars.json` );
 const SpotifyWebApi = require('spotify-web-api-node');
+const {Dropbox} = require(`dropbox`); // eslint-disable-line no-unused-vars
 
 const spotifyApi = new SpotifyWebApi({
   clientId: spotifyID,
@@ -16,9 +24,27 @@ const spotifyApi = new SpotifyWebApi({
   refreshToken: spotifyRefresh,
 });
 
+const dropboxConfig = {
+  refreshToken: dropboxRefresh,
+  clientId: dropboxClientId,
+  clientSecret: dropboxClientSecret,
+};
+
+const dbx = new Dropbox(dropboxConfig);
+
 // Create a new Discord client instance
 const client = new Client({intents: [Intents.FLAGS.GUILDS,
   Intents.FLAGS.GUILD_MESSAGES]});
+
+/**
+ * Grabs the song ID from Spotify URLs
+ * @param {string} input The Spotify Track URL
+ * @return {string} the Spotify track ID
+ */
+function useRegex(input) {
+  const regex = /(?<=track\/)[^?\n]+/i;
+  return input.match(regex);
+}
 
 /**
  * Takes care of Spotify authorization.
@@ -84,10 +110,64 @@ async function spotifyBot(msg) {
   }
 }
 
-// For troubleshooting. Really doesn't need to be async but it is.
+/**
+ * Mascot Bot
+ */
+async function masBot() {
+  setInterval(async () => {
+    // Fetch channels and save them in a const
+    const Mascot = await client.channels.fetch(mascotchannelId).catch();
+    const Dan = await client.users.fetch(danId).catch();
+    // Make sure they were fetched properly and continue
+    if ( Mascot ) {
+      dbx.filesListFolder({path: dropboxFolder})
+          .then((fileList) => {
+            const fileName = fileList.result.entries[0].name;
+            const filePath = fileList.result.entries[0].path_lower;
+            if ( fileName && filePath ) {
+              dbx.filesGetTemporaryLink({path: filePath})
+                  .then((fileLink) => {
+                    const Link = fileLink.result.link;
+                    const mascotMessage = Mascot.send({
+                      files: [{
+                        attachment: Link,
+                        name: fileName,
+                      }],
+                    }).catch((err) => {
+                      console.error(err);
+                    });
+                    if ( mascotMessage ) {
+                      dbx.filesDeleteV2({path: filePath})
+                          .then((response) => {
+                            console.log('Succesfully deleted file: ' +
+                            response.result.metadata.path_display);
+                          })
+                          .catch((err) => {
+                            console.error(err);
+                          });
+                    }
+                  })
+                  .catch((err) => {
+                    console.error(err);
+                  });
+            }
+          })
+          .catch((err) => {
+            console.error(err);
+            Mascot.send({
+              content: `${Dan} Man what the fuck there are no images left.`,
+            });
+          });
+    } else {
+      console.log('Couldn\'t find that channel man, shit sucks big time.');
+    }
+  }, globalInterval);
+}
+
 client.once(`ready`, (async ()=>{
   console.log(`Ready`);
   spotifyToken();
+  masBot();
 }));
 
 // Fires on every message
@@ -116,14 +196,3 @@ client.on(`error`, async (error) => {
 client.on(`unhandledReject`, console.log);
 
 client.login(token);
-
-/**
- * Grabs the song ID from Spotify URLs
- * @param {string} input The Spotify Track URL
- * @param {const} regex The regex used to extract the Spotify Track ID
- * @return {string} the Spotify track ID
- */
-function useRegex(input) {
-  const regex = /(?<=track\/)[^?\n]+/i;
-  return input.match(regex);
-}
